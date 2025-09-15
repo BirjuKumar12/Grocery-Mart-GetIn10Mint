c2d821c159946b2d4b17fa3309d0f33c051f6567
BirjuKumar12 <shiyaram644@gmail.com>
2025-09-15 13:17:25 +0530

    Fixed ALLOWED_HOSTS for Render deployment

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -12,7 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-ALLOWED_HOSTS = ['grocery-mart-getin10mint.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    "grocery-mart-getin10mint.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
+
 
 
 
